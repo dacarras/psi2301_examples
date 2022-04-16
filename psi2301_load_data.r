8d9dc0a31409993712ff9050a33546c14c79477f
dacarras <dacarras@gmail.com>
2022-04-15 23:10:33 -0400

    upload load data

---
+++ psi2301_load_data.r
@@ -0,0 +1,43 @@
+
+# acciones colectivas
+data_ac <- read.csv(
+	url(
+'https://raw.githubusercontent.com/dacarras/psi2301_examples/master/data/accionescolestivas.csv'
+	)
+)
+
+# ptsd
+data_ptsd <- read.csv(
+	url(
+'https://raw.githubusercontent.com/dacarras/psi2301_examples/master/data/ptsd_data.csv'
+	)
+)
+
+# homonegatividad
+data_hom <- read.csv2(
+	url(
+'https://raw.githubusercontent.com/dacarras/psi2301_examples/master/data/homonegatividad.csv'
+	)
+)
+
+# integracion universitaria
+data_int <- read.csv(
+	url(
+'https://raw.githubusercontent.com/dacarras/psi2301_examples/master/data/integracion_universitaria.csv'
+	)
+)
+
+# desarollo psicomotor
+data_psi <- read.csv(
+	url(
+'https://raw.githubusercontent.com/dacarras/psi2301_examples/master/data/desarollo_psicomotor.csv'
+	)
+)
+
+# terce
+data_terce <- read.csv(
+	url(
+'https://raw.githubusercontent.com/dacarras/psi2301_examples/master/data/terce_n.csv'
+	)
+)
+
