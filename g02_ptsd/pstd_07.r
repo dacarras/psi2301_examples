d26adba5b39bd1f0b421e1828e1dbbbd3a152953
dacarras <dacarras@gmail.com>
2022-06-15 16:06:57 -0400

    update ptsd_07

--- g02_ptsd/pstd_07.r
+++ g02_ptsd/pstd_07.r
@@ -8,7 +8,6 @@ data_ptsd <- read.csv(
 	)
 )
 
-
 #--------------------------------------
 # grafico con recategorización
 #--------------------------------------
@@ -18,15 +17,15 @@ dplyr::count(data_ptsd, fut2)
 # variable 2: "Experiencia personal de trauma de guerra"
 # fut2 = "What do you think; will there be another civil 
 #         war in Sierra Leone within the next 10 years"
-# grafico de barras con 4 categorias.
+# grafico de barras recategorizado 4 categorias.
 
 data_ptsd %>%
 mutate(fut2_4_cat = case_when(
-	fut2 == 1 ~ 1,
-	fut2 == 2 ~ 2,
-	fut2 == 3 ~ 3,
-	fut2 == 4 ~ 4,
-	fut2 == 5 ~ 4
+	fut2 == 1 ~ 1, # certainly -> probablemente
+	fut2 == 2 ~ 1, # probably  -> probablemente
+	fut2 == 3 ~ 2, # undecided -> no lo sé
+	fut2 == 4 ~ 3, # probably not  -> probablemente no
+	fut2 == 5 ~ 4  # certainly not -> no
 	)) %>%
 with(., table(fut2_4_cat)) %>%
 barplot(.,
